c5d6c0668f19d51784dcd144edd8dc57e58942ee
Mohammed Natour <bgd4500@gmail.com>
2023-10-05 10:40:20 +0300

    add hilt and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+//    alias(libs.plugins.androidApplication)
+//    alias(libs.plugins.kotlinAndroid)
+//    id("com.google.dagger.hilt.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -31,11 +36,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -51,9 +56,6 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
-    implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
@@ -70,4 +72,18 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //noinspection UseTomlInstead
+    implementation( "androidx.compose.material:material-icons-extended")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+//    alias(libs.plugins.androidApplication) apply false
+//    alias(libs.plugins.kotlinAndroid) apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
+//buildscript {
+//    repositories {
+//        // other repositories...
+//        mavenCentral()
+//    }
+//    dependencies {
+//        // other plugins...
+//        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+//    }
+//}
 true // Needed to make the Suppress annotation work for the plugins block
