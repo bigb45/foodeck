753c2b2b4a32b7a48deb2e1d013b333fa62de1fc
Mohammed Natour <bgd4500@gmail.com>
2024-01-21 22:34:10 +0300

    feature: add "quantity" and "instructions" sections to meals screen

--- feature/home/src/main/java/com/example/menu_item/MenuItemScreen.kt
+++ feature/home/src/main/java/com/example/menu_item/MenuItemScreen.kt
@@ -1,26 +1,44 @@
 package com.example.menu_item
 
 import android.util.Log.d
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Remove
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.compose.gray2
+import com.example.compose.gray6
+import com.example.core.ui.theme.Typography
+import com.example.core.ui.theme.interBold
 import com.example.restaurant.CollapsingToolbar
 import com.example.restaurant.MAX_TOOLBAR_HEIGHT
 import com.example.restaurant.MIN_TOOLBAR_HEIGHT
@@ -37,13 +55,12 @@ fun MenuItemScreen(onNavigateUp: () -> Unit) {
     val scope = rememberCoroutineScope()
     val lazyListState = rememberLazyListState()
 
-    val nestedScrollConnection =
-        rememberCustomNestedConnection(toolbarState, lazyListState, scope)
+    val nestedScrollConnection = rememberCustomNestedConnection(toolbarState, lazyListState, scope)
     val viewModel: MenuItemViewModel = hiltViewModel()
 
     Column(
-        modifier = Modifier
-            .nestedScroll(nestedScrollConnection)
+        modifier = Modifier.nestedScroll(nestedScrollConnection)
+            .background(gray6)
     ) {
 
         CollapsingToolbar(
@@ -54,25 +71,118 @@ fun MenuItemScreen(onNavigateUp: () -> Unit) {
             progress = toolbarState.progress,
             onNavigateUp = onNavigateUp,
             expandedActions = { MealActions() },
-            collapsedActions = { IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Outlined.Search, null)
-            }},
+            collapsedActions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Outlined.Search, null)
+                }
+            },
             title = "Meal name",
             subTitle = "Restaurant name, address",
-        ){
+        ) {
 
         }
         LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth(),
-            state = lazyListState
+            modifier = Modifier.fillMaxWidth(),
+            state = lazyListState,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(100) {
-                Text(viewModel.menuItemId)
+            item {
+                Counter(modifier = Modifier,
+                    counter = viewModel.counter.collectAsState().value,
+                    increment = { viewModel.incrementCounter() },
+                    decrement = { viewModel.decrementCounter() })
+            }
+            item {
+                Instructions(onTextChange = { /*TODO*/ }, text = "")
             }
         }
     }
 
+}
+
+
+@Composable
+fun Counter(
+    modifier: Modifier = Modifier,
+    counter: Int,
+    increment: () -> Unit,
+    decrement: () -> Unit,
+
+    ) {
+    Column(
+        modifier = modifier
+            .background(colorScheme.surface)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text("Quantity", style = Typography.titleLarge.copy(fontFamily = interBold))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(width = 1.dp, shape = RoundedCornerShape(16.dp), color = gray6)
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            IconButton(onClick = { decrement() }, modifier = Modifier.weight(1f)) {
+                Icon(imageVector = Icons.Outlined.Remove, contentDescription = null, tint = gray2)
+            }
+            Text(
+                counter.toString(),
+                style = Typography.titleLarge,
+                modifier = Modifier
+                    .weight(5f)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            IconButton(
+                onClick = { increment() }, modifier = Modifier.weight(1f)
+            ) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = null, tint = gray2)
+            }
+        }
+    }
+}
+
+@Composable
+fun Instructions(
+    modifier: Modifier = Modifier,
+    onTextChange: () -> Unit,
+    text: String,
+) {
+    Column(
+        modifier = modifier
+            .background(colorScheme.surface)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text("Instructions", style = Typography.titleLarge.copy(fontFamily = interBold))
+        Column(
+            modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(16.dp)
+//            horizontalArrangement = Arrangement.Center,
+//            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                "Let us know if you have specific things in mind",
+                style = Typography.bodyLarge.copy(color = gray2)
+            )
+            OutlinedTextField(modifier = Modifier
+                .fillMaxWidth()
+                .border(width = 1.dp, shape = RoundedCornerShape(16.dp), color = gray6)
+                .height(54.dp),
+                value = text,
+                onValueChange = { onTextChange() },
+                shape = RoundedCornerShape(16.dp),
+                placeholder = {
+                    Text(
+                        "e.g. less spices, no mayo etc",
+                        style = Typography.titleMedium.copy(color = gray2)
+                    )
+                }
+
+            )
+        }
+    }
 }
 
 @Composable
--- feature/home/src/main/java/com/example/menu_item/MenuItemViewModel.kt
+++ feature/home/src/main/java/com/example/menu_item/MenuItemViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.menu_item
 
+import androidx.compose.runtime.IntState
+import androidx.compose.runtime.asIntState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.menu_item.navigation.menuItemIdArgument
 import com.example.restaurant.navigation.restaurantIdArgument
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import java.net.URLDecoder
 import javax.inject.Inject
 
@@ -12,7 +18,18 @@ import javax.inject.Inject
 class MenuItemViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ): ViewModel() {
+    private val _counter = MutableStateFlow(1)
+    val counter: StateFlow<Int> = _counter.asStateFlow()
     val menuItemId: String =
         URLDecoder.decode(savedStateHandle[menuItemIdArgument], Charsets.UTF_8.name())
+
+    fun incrementCounter() {
+        _counter.value += 1
+    }
+    fun decrementCounter() {
+        if(_counter.value > 1){
+            _counter.value -= 1
+        }
+    }
 
 }
